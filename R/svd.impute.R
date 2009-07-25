c1b4665bc062a755eef50bd57ccf9f63e78bcf8b
Patrick Perry <patperry@gmail.com>
2009-07-25 14:18:15 -0400

    Add R implementation of SVDimpute

---
+++ R/svd.impute.R
@@ -0,0 +1,86 @@
+
+svd.impute.check <- function( impute ) {
+    function( x, k=min(n,p), tol=1e-10, maxiter=1000 ) {
+        x  <- as.matrix( x )
+        n  <- nrow( x )
+        p  <- ncol( x )
+        np <- min( n, p )
+    
+        if( !is.numeric( x ) )
+            stop( "'x' must be a numeric type, not '", class( x ), "'." )
+        if( !all( is.finite( x ) | is.na( x ) ) )
+            stop( "'x' cannot contain any infinities or NaNs." )
+        if( !( k >= 0 ) )
+            stop( "'k' must be in the non-negative, not '", k, "'." )
+        if( !( k <= np ) )
+            stop( "'k' must be less than '", np, "' not '", k, "'." )
+        if( !( tol > 0 ) )
+            stop( "'tol' must be positive, not, '", tol, "'." )
+        if( !( maxiter > 0 ) )
+            stop( "'maxiter' must be a positive value, not '", 
+                  maxiter, "'." )
+    
+        res <- impute( x, k, tol, maxiter )
+        
+        if( res$iter == maxiter )
+            warning( "Did not converge in '", maxiter, "' iterations." )
+            
+        res
+    }
+}
+
+impute.colMeans <- function( x ) {
+    isna      <- is.na( x )
+    x[ isna ] <- 0
+    
+    if( min( dim( x ) ) > 0 ) {
+        s  <- drop( matrix( 1, 1, nrow( x ) ) %*% x )
+        n  <- apply( !isna, 2, sum )
+        mu <- ifelse( n == 0, 0, s/n )
+    } else {
+        mu <- rep( 0, ncol( x ) )
+    }
+    
+    mu
+}
+
+svd.impute.R.unchecked <- function( x, k, tol, maxiter )
+{
+    n <- nrow( x )
+    p <- ncol( x )
+    K <- seq_len( k )
+      
+    missing     <- is.na( x )
+    missing.idx <- which( missing )
+    missing.j   <- ( ( missing.idx-1 ) %/% n ) + 1
+
+    mu                   <- impute.colMeans( x )
+    missing.est          <- mu[ missing.j ]
+    xhat0                <- x
+    xhat0[ missing.idx ] <- missing.est
+    
+    iter  <- 0
+    delta <- Inf
+    
+    while( ( delta > tol ) && ( iter < maxiter ) ) {
+        xhat0.svd <- svd( xhat0 )
+        uhat      <- xhat0.svd$u[ ,K,drop=FALSE ] 
+        dhat      <- xhat0.svd$d[ K ] 
+        vhat      <- xhat0.svd$v[ ,K,drop=FALSE ]
+        
+        xhat1 <- uhat %*% ( dhat * t( vhat ) )
+        
+        delta <- ( sum( ( ( xhat0-xhat1 )[ !missing ] )^2 )
+                   /
+                   ( .Machine$double.eps + sum( ( xhat0[ !missing ] )^2 ) ) )
+        iter  <- iter + 1
+        xhat0 <- xhat1
+    }
+        
+    x[ missing ] <- xhat0[ missing ]
+    
+    list( imputed=missing.idx, x=x, u=uhat, d=dhat, v=vhat, 
+          iter=iter, delta=delta )
+}
+
+svd.impute.R <- svd.impute.check( svd.impute.R.unchecked )
