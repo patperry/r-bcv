7a8e42b07d24d2cf455b3aee970db2ef509bb581
Patrick Perry <patperry@gmail.com>
2009-07-30 16:48:23 -0400

    Add default plot method for cvsvd

---
+++ R/plot.R
@@ -0,0 +1,42 @@
+
+plot.cvsvd <- function( x, errorbars=TRUE,
+                        col="blue", col.errorbars="gray50", 
+                        add=FALSE, xlab="Rank", ylab="Prediction Error",
+                         ... ) {
+    press   <- x$press
+    maxrank <- x$maxrank
+    
+    K           <- nrow( press )
+    rank        <- seq( from=0, to=maxrank, by=1 )
+    press.mean  <- apply( press, 1, mean )
+    press.se    <- apply( press, 1, sd ) / sqrt( K )
+    
+    if( !add ) {
+        if( errorbars ) {
+            plot( c(rank-0.2,rank+0.2), press.mean+c(-press.se, press.se), 
+                  t='n', xlab=xlab, ylab=ylab, ... )
+        } else {
+            plot( rank, press.mean, t='n', xlab=xlab, ylab=ylab, ... )
+        }
+    }
+
+    lines( rank, press.mean, col=col )
+    
+    if( errorbars ) {
+        segments( rank-0.2, press.mean-press.se, 
+                  rank+0.2, press.mean-press.se,
+                  col=col.errorbars )
+
+        segments( rank, press.mean-press.se, 
+                  rank, press.mean+press.se,
+                  col=col.errorbars )
+
+        segments( rank-0.2, press.mean+press.se, 
+                  rank+0.2, press.mean+press.se,
+                  col=col.errorbars )
+    }
+
+    points( rank, press.mean, col=col, pch=16, cex=0.6 )
+    
+    invisible( list( k=rank, press=press.mean ) )
+}
